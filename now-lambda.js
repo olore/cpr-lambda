e911618049f07ecf439c43f976eb819974525c8a
Brian Olore <brian@olore.net>
2019-03-05 22:34:16 -0500

    swapped out azure for retext

--- now-lambda.js
+++ now-lambda.js
@@ -1,88 +1,65 @@
 'use strict';
-let https = require ('https');
-
-// https://westcentralus.api.cognitive.microsoft.com/text/analytics
-// https://westcentralus.api.cognitive.microsoft.com/text/analytics
-// https://westcentralus.api.cognitive.microsoft.com/text/analytics
-// Key 1: b3d3818c7c844f5996a2a62ec6e0d70b
-// Key 2: 95b375ffc4d44eac803fab2e67b654d5
-
 const {json} = require('micro');
+
+var vfile = require('to-vfile')
+var unified = require('unified')
+var english = require('retext-english')
+var sentiment = require('retext-sentiment')
+var tmp = require('tmp');
+
+var processor = unified()
+  .use(english)
+  .use(sentiment)
 
 module.exports = async (req, res) => {
 	const data = await json(req);
-	console.log(data);
   let commentText = data.text;
-  let documents = {
-    documents: [
-      { 
-        'id': '1', 
-        'language': 'en', 
-        'text': commentText
-      }
-    ]
-  };
 
-  return get_sentiments (documents, function(resp) {
-    console.log('finally got', resp);
-    let obj = JSON.parse(resp);
-    res.end(JSON.stringify({score: obj.documents[0].score}));
+  let tmpobj = tmp.fileSync();
+  let filename = tmpobj.name;
+  let file = vfile({path: filename, contents: commentText})
 
-  });
+  let tree = processor.parse(file)
+  processor.run(tree, file)
+  // console.log(tree.data); // { polarity: -4, valence: 'negative' }
+
+  let score = 0;
+  switch(tree.data.polarity) {
+    case -5: 
+      score = 0;
+      break;
+    case -4: 
+      score = 0.10;
+      break;
+    case -3: 
+      score = 0.20;
+      break;
+    case -2: 
+      score = 0.30;
+      break;
+    case -1: 
+      score = 0.40;
+      break;
+    case 0: 
+      score = 0.50;
+      break;
+    case 1: 
+      score = 0.60;
+      break;
+    case 2: 
+      score = 0.70;
+      break;
+    case 3: 
+      score = 0.80;
+      break;
+    case 4: 
+      score = 0.90;
+      break;
+    case 5: 
+    case 6:  // "i love this. it's the best" (sposta only go up to 5!)
+      score = 1.00;
+      break;
+  }
+  res.end(JSON.stringify({score: score}));
 
 }
-
-
-// **********************************************
-// *** Update or verify the following values. ***
-// **********************************************
-
-// Replace the accessKey string value with your valid access key.
-let accessKey = 'b3d3818c7c844f5996a2a62ec6e0d70b';
-
-// Replace or verify the region.
-
-// You must use the same region in your REST API call as you used to obtain your access keys.
-// For example, if you obtained your access keys from the westus region, replace 
-// "westcentralus" in the URI below with "westus".
-
-// NOTE: Free trial access keys are generated in the westcentralus region, so if you are using
-// a free trial access key, you should not need to change this region.
-let uri = 'westcentralus.api.cognitive.microsoft.com';
-let path = '/text/analytics/v2.0/sentiment';
-
-let response_handler = function (response, cb) {
-    let body = '';
-    response.on ('data', function (d) {
-        body += d;
-    });
-    response.on ('end', function () {
-        let body_ = JSON.parse (body);
-        let body__ = JSON.stringify (body_, null, '  ');
-        console.log (body__);
-        cb(body__);
-    });
-    response.on ('error', function (e) {
-        console.log ('Error: ' + e.message);
-    });
-};
-
-let get_sentiments = function (documents, cb) {
-    let body = JSON.stringify (documents);
-
-    let request_params = {
-        method : 'POST',
-        hostname : uri,
-        path : path,
-        headers : {
-            'Ocp-Apim-Subscription-Key' : accessKey,
-        }
-    };
-
-    let req = https.request (request_params, function(x) {
-      response_handler(x, cb);
-    });
-    req.write (body);
-    req.end ();
-}
-
